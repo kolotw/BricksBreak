9bcb70193905d145c6a5044677d5f2146783e3ed
kolotw <77420523+kolotw@users.noreply.github.com>
2025-01-10 20:21:31 +0800

    0110-6

--- Assets/csvReader/processCSV.cs
+++ Assets/csvReader/processCSV.cs
@@ -174,7 +174,7 @@ public class processCSV : MonoBehaviour
         if (Application.platform == RuntimePlatform.WebGLPlayer)
         {
             // WebGL 平台使用 UnityWebRequest
-            filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+            //filePath = Path.Combine(Application.streamingAssetsPath, fileName);
             GameObject.Find("/Canvas/filepath").GetComponent<Text>().text = filePath;
             UnityWebRequest request = UnityWebRequest.Get(filePath);
             yield return request.SendWebRequest();
